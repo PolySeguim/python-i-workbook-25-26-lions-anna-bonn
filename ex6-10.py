3f77bc6202388d3213c39497b8b65d86fd825e87
anna-bonn <bonnanna08@gmail.com>
2025-09-29 10:30:48 -0400

    09-29

--- ex6-10.py
+++ ex6-10.py
@@ -9,6 +9,15 @@ both the tax and the tip.  Format the output so that all of the values
 are displayed using two decimal places.  (17 lines)
 """
 
+def ex6():
+    mealCost = float(input("How much was your meal? "))
+    mealTax = mealCost * 1.0575
+    mealTip = mealCost * 0.18
+    totalCost = mealTax + mealTip
+
+    print(totalCost)
+
+
 """
 Exercise 7:  Sum of the First n Positive Integers
 Write a program that reads a positive integer, n, from the user and 
@@ -18,6 +27,10 @@ sum = (n*(n+1))/2
 (12 lines)
 """
 
+def ex7():
+    n =float(input("Input a positive integer. "))
+    sum = (n*(n+1))/2
+
 """
 Exercise 8:  Widgets and Gizmos
 An online retailer sells two products:   widgets and gizmos.  Each widget 
@@ -26,6 +39,20 @@ the number of gizmos in an order from the user.  Then your program should
 compute and display the total weight of the order.  (15 lines)
 
 """
+
+def weight():
+    widget = 75
+    gizmo = 112
+    orderGizmo = float(input("How many gizmos did you order? "))
+    orderWidget = float(input("How many widgets did you order? "))
+    widgetWeight = (orderWidget * widget)
+    gizmoWeight = (orderGizmo * gizmo)
+    totalWeight = (widgetWeight + gizmoWeight,  "grams")
+    print (totalWeight)
+    
+#weight()
+
+
 
 """
 Exercise 9:  Compound Interest
@@ -37,6 +64,18 @@ your program should compute and display the amount in the savings account after
 1, 2, and 3 years.  Display each amount so that it is rounded to 2 decimal 
 places.  (19 lines)
 """
+
+def interest():
+    p = float(input("How much money are you entering? "))
+    n = 1
+    r = 0.04
+    t = [1,2,3]
+    
+    for i in t:
+        a = p*(1+(r/n))**(n*i)
+        #print(i, type(i))
+        print(a)
+
 
 """
 Exercise 10:  Arithmetic
@@ -53,6 +92,19 @@ should compute and display:
 Hint:  you will probably find the log10 function in the math module helpful
 for computing the second last item in the list.
 """
+
+def arithmetic():
+    a = float(input("What is the value of a?"))
+    b = float(input("What is the value of b?"))
+    sum = (a+b)
+    diff = (a-b)
+    quot = (a/b)
+    remain = (a%b)
+    log10 = math.log(a)
+    expo = a**b
+    print(sum, diff, quot, remain, log10, expo)
+    
+arithmetic()  
 
 if __name__ == "__main__":
     print("Hi there!")
